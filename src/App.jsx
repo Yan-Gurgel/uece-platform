7e5794d4773267f175c5732a60fd1b569ff737b4
Yan Gurgel <yan.muquest@gmail.com>
2022-10-16 19:58:46 -0300

    Aplicação de estilo global

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./global.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>My Platform</h1>
+    <div>
+      <Header />
+      <Post
+        author="Yan Gurgel"
+        content=" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, doloribus veniam. Debitis vitae animi ab omnis pariatur libero consequuntur dolorum quos cupiditate illo unde, asperiores ipsa dolore. Voluptas, fugiat ullam."
+      />
     </div>
   );
 }
