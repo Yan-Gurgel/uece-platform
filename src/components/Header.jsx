d80d21353dcd829d8da9e6367601bc77f29a8b1e
Yan Gurgel <yan.muquest@gmail.com>
2022-10-19 20:11:13 -0300

    Add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import styles from "./Header.module.css";
 
+import brasaoUece from '../assets/brasao-uece.svg';
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>UECE PLATFORM</strong>;
+      <img src={brasaoUece} alt="Brasão da UECE" />
+      <strong>PLATAFORMA DE ESTUDOS DA UECE</strong>
+      <img src={brasaoUece} alt="Brasão da UECE" />
     </header>
   );
 }
