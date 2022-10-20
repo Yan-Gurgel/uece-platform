3d8a3db95462090e9494784f0f2ae068a99eba27
Yan Gurgel <yan.muquest@gmail.com>
2022-10-19 21:47:46 -0300

    Add Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Yan Gurgel"
-        content=" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, doloribus veniam. Debitis vitae animi ab omnis pariatur libero consequuntur dolorum quos cupiditate illo unde, asperiores ipsa dolore. Voluptas, fugiat ullam."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Yan Gurgel"
+            content=" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, doloribus veniam. Debitis vitae animi ab omnis pariatur libero consequuntur dolorum quos cupiditate illo unde, asperiores ipsa dolore. Voluptas, fugiat ullam."
+          />
+          <Post
+            author="Yan Gurgel"
+            content=" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, doloribus veniam. Debitis vitae animi ab omnis pariatur libero consequuntur dolorum quos cupiditate illo unde, asperiores ipsa dolore. Voluptas, fugiat ullam."
+          />
+        </main>
+      </div>
     </div>
   );
 }
